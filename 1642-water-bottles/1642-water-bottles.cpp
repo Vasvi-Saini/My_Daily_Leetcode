b6bcf98303ac1c4f3b6cdd0a83e1a1e39d74df2f
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-07-07 23:34:50 +0530

    Time: 2 ms (53.84%), Space: 7 MB (72.3%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int consumed = 0;
-        while(numBottles >= numExchange){    
-            consumed += numExchange;
-            numBottles -= numExchange;
-            numBottles += 1;
-        }
-        return consumed + numBottles;
+       int consume = 0;
+       while(numBottles>=numExchange){        //enough bottles to exchange atleast one bottle
+        consume += numExchange;              // increase thee count of bottle exchanges
+        numBottles -= numExchange;          // decrease the count of bottle by no. of bottle exchanged
+        numBottles++;   //add 1 bottle bcz i just exchanged numExchange empty bottles for 1bottle                   
+       }
+        return consume + numBottles; 
     }
 };
