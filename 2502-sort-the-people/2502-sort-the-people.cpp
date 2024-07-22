1e01408d472852c588dd81664318eca140e0a118
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-07-22 22:39:25 +0530

    Time: 34 ms (37.13%), Space: 27.5 MB (34.51%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        map<int , string> mpp;
+          map<int , string> mpp;
         for(int i = 0; i<names.size(); i++){
             mpp[heights[i]] = names[i];
         }
