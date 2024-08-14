69598d7505e436bdad83441d9281f2360084794b
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-08-14 11:24:29 +0530

    Time: 3 ms (38.12%), Space: 8.3 MB (41.72%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,13 +1,13 @@
  class Solution {
 public:
     int divide(int dividend, int divisor) {
-        long long ans = (long)dividend/divisor;
-        if(ans < INT_MIN){
-            return -2e31;
-        }
-        else if(ans > INT_MAX){
-            return 2147483648-1;
-        }
-        return ans;
+      long long ans = (long long ) dividend/ divisor;
+      if(ans<INT_MIN){
+        return INT_MIN;
+    }
+    else if(ans>INT_MAX){
+        return INT_MAX;
+    }
+    return (int)ans;
     }
 };
