dc96e9ea092f83d76346005ca34704b73fb54140
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-08-17 23:42:32 +0530

    Time: 233 ms (53.3%), Space: 107.9 MB (57.08%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        int maxi = INT_MIN , mini = INT_MAX , idx;
+        for(int i = 0; i<arrays.size(); i++){
+            for(int j = 0; j<arrays[i].size(); j++){
+                if(arrays[i][j] > maxi){
+                    maxi = arrays[i][j];
+                    idx = i;
+                }
+            }
+        }
+        
+        for(int i = 0; i<arrays.size(); i++){
+            for(int j = 0; j<arrays[i].size(); j++){
+                if(arrays[i][j] < mini && i!=idx){
+                    mini = arrays[i][j];
+                }
+            }
+        }
+
+        int maxi2  = INT_MIN , mini2 = INT_MAX , idx2;
+        for(int i = 0; i<arrays.size(); i++){
+            for(int j = 0; j<arrays[i].size(); j++){
+                if(arrays[i][j] < mini2){
+                    mini2 = arrays[i][j];
+                    idx2 = i;
+                }
+            }
+        }
+        
+        for(int i = 0; i<arrays.size(); i++){
+            for(int j = 0; j<arrays[i].size(); j++){
+                if(arrays[i][j] > maxi2 && i!=idx2){
+                    maxi2 = arrays[i][j];
+                }
+            }
+        }
+        return max(abs(maxi - mini) ,abs(maxi2-mini2));
+    }
+};
+
+ 
