aeb04572673359a804058cb8f672009950a12ddd
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-09-02 00:00:38 +0530

    Time: 673 ms (100%), Space: 288.4 MB (100%) - LeetHub

---
+++ 3495-k-th-nearest-obstacle-queries/3495-k-th-nearest-obstacle-queries.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<vector<int>>& queries, int k) {
+        vector<int> ans;
+        vector<int> res;
+        priority_queue<int> pq;
+        for(auto i : queries){
+            pq.push(abs(i[0]) + abs(i[1]));
+            if(pq.size()>k){
+                pq.pop();
+            }
+            if(pq.size() == k){
+                res.push_back(pq.top());
+            }
+            else{
+                res.push_back(-1);
+            }
+        }
+        return res;
+    }
+};
