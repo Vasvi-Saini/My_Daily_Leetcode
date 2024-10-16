e33f322a56e23c81aa721796106ab4e645bc28cd
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-10-16 23:33:42 +0530

    Time: 3 ms (43.32%), Space: 7.5 MB (90.57%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -1,6 +1,42 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
+        // string ans = "";
+
+        // priority_queue<pair<int , char>> pq;
+        // if(a>0) pq.push({a , 'a'});
+        // if(b>0) pq.push({b , 'b'});
+        // if(c>0) pq.push({c , 'c'});
+        // while(!pq.empty()){
+        //     int currFreq  = pq.top().first;
+        //     int currChar = pq.top().second;
+        //     pq.pop();
+
+        //     if(ans.size() >= 2 && ans[ans.size()-1] == currChar && ans[ans.size()-2] == currChar){
+        //         if(pq.empty()){
+        //             break;
+        //         }
+        //         int nextCount = pq.top().first;
+        //         char nextChar = pq.top().second;
+        //         pq.pop();
+        //         ans.push_back(nextChar);
+        //         nextCount--;
+        //         if(nextCount > 0){
+        //         pq.push({nextCount , nextChar});
+        //         ans.push_back(nextChar);
+        //         }
+        //         else{
+        //             currFreq--;
+        //             ans.push_back(currChar);
+        //         }
+
+        //         if(currFreq > 0){
+        //             pq.push({currFreq , currChar});
+        //         }
+        //     }
+        // }
+        // return ans;
+
         priority_queue<pair<int , char>> pq;
         if(a>0) pq.push({a , 'a'});
         if(b>0) pq.push({b , 'b'});
