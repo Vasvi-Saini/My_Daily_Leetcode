41d376864680b2d1a875a9f2beb5511f03ea6daa
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-10-22 11:01:48 +0530

    Time: 6 ms (82.52%), Space: 10.3 MB (81.96%) - LeetHub

--- 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int numberOfSubstrings(string s, int k) {
         unordered_map<char , int> mp;
-        int i=0,j=0;
-        int count = 0, n = s.size();
-
-        while(j<n) { 
-            mp[s[j]]++;
-            while(mp[s[j]]>=k) {
-                count += n-j;
+        int i = 0 , j = 0, count = 0;
+        int n = s.size();
+       while(j<n){
+        mp[s[j]]++;
+           while(mp[s[j]] >= k){
+             count += n-j;
                 mp[s[i]]--;
                 i++;
-            }
-            j++;
-        }
-        return count;
+             }
+        j++;
+       }
+       return count;
     }
 };
