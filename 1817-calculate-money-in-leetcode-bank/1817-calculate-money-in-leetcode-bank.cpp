d489e457ea4c7d04a22da66183497089e424a5a8
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-11-07 15:49:50 +0530

    Time: 0 ms (100%), Space: 7.3 MB (84.04%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -4,16 +4,10 @@ public:
         int noOfWeeks = n/7;
         int count = 0;
         for(int i=1; i<=noOfWeeks; i++){
-            for(int j=i; j<i+7; j++){   // 7 days
-               count += j;
-            }
+           count += 7*(2*i+6)/2;
         }
         int dayLeft = n%7;
-        int k = noOfWeeks + 1;
-        while(dayLeft--){
-            count += k;
-            k++;
-        }
+        count += dayLeft * (2*(n/7 + 1) + dayLeft - 1)/2;
         return count;
     }
 };
