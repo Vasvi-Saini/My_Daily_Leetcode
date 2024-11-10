bd313905bced38323977727a39749e4e16d766b5
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-11-10 23:44:19 +0530

    Time: 2 ms (100%), Space: 7.5 MB (39.57%) - LeetHub

---
+++ 2630-alternating-digit-sum/2630-alternating-digit-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+         int rev = 0;
+
+        while(n){
+            rev = (rev*10)+n%10;
+            n/=10;
+        }
+
+        int i = 0 , ans = 0;
+        while(rev){
+            if(i%2 == 0){
+                ans += rev%10;
+                rev /= 10;
+                i++;
+            }
+            else{
+                ans -= rev%10;
+                rev /= 10;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
