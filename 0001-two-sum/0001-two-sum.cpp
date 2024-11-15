50a767524fe87d3fe550db169c0c7e35ac58f9cf
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-11-15 20:24:19 +0530

    Time: 3 ms (79.15%), Space: 14.6 MB (16.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        vector<int> ans;
-        for(int i=0; i<nums.size()-1; i++){
-           for( int j= i+1; j<nums.size(); j++){
-            if(nums[i] + nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-               break;
-           }
+        map<int,int> memo;
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (memo.contains(diff)) {
+                return vector<int> {memo[diff], i};
+            }
+            memo[nums[i]] = i; 
         }
-     }  
-        return ans;
+        return vector<int> {};
     }
 };
