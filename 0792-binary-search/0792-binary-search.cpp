cfed4d29de901f290609da3d178a071824da9e35
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-11-17 19:41:40 +0530

    Time: 0 ms (100%), Space: 30.2 MB (24.21%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int n = arr.size();
-        int start =0, end = n-1, mid;
+    int binary(vector<int> &arr, int low, int high, int target){
+     if(low > high) return -1;
 
-        while(start<=end){
-            mid =start+(end-start)/2;    // mid ko find kro
-            if(arr[mid]==target) return mid;
-            else if(arr[mid]<target) start = mid + 1;
-            else end = mid - 1;      //arr[mid]>target
-        }
-        return -1;
+    int mid = low + (high-low) / 2;
+
+    if(arr[mid] == target) return mid;
+
+    else if(arr[mid] < target) return binary(arr, mid+1, high, target);
+
+    else return binary(arr, low, mid-1, target);
+
+    }
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        return binary(nums, 0, n-1, target);
     }
 };
