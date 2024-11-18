93f5dd44d6138dd8c6e2e5672ec3003aa832dd82
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-11-18 21:47:08 +0530

    Time: 3 ms (28.48%), Space: 8.9 MB (81.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,38 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int s_length = s.length();
-        int s_index = s.length() - 1;
+        int first = 0;
+        int last = s.length() - 1;
+      
+        while(first <= last){
+
+            while(
+                first < last 
+                && !(((s[first] >= 'a') && (s[first] <= 'z'))
+                || ((s[first] >= 'A') &&  (s[first] <= 'Z'))
+                || ((s[first] >='0') &&  (s[first] <= '9')))
+            ) {
+                first++;
+            }
+
+            while(
+                first < last 
+                && !(((s[last] >= 'a') && (s[last] <= 'z'))
+                || ((s[last] >= 'A') && (s[last] <= 'Z')) 
+                ||  ((s[last] >='0') &&  (s[last]  <= '9')))
+                ) 
+    {
+        last--;
+    }
          
-         string result = " ";
+     
+    
+        if(tolower(s[first]) != tolower(s[last])) return false;
+        first++;
+        last--;
 
-         if(s_length <= 1) return true;
-        
-           int first = 0;
-           int last = s_index;
 
-           while(first <= last){
-              while (first < last &&!isalnum(s[first])) first++;
-              while(first < last && !isalnum(s[last])) last--;
-              if (tolower(s[first]) != tolower(s[last])) {
-                return false;
-           }
-           first++;
-           last--;
-        }
-        
-          return true;
-        
+      }
+      return true;
     }
 };
