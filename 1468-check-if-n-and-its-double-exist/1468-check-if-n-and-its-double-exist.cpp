ebef4933d8c18b0af16ee5296c6e6b68a70c725c
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-12-01 20:11:01 +0530

    Time: 3 ms (24.95%), Space: 14.8 MB (10.47%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -3,10 +3,11 @@ public:
     bool checkIfExist(vector<int>& arr) {
         unordered_map<int, int> mp;
         for(int i=0; i<arr.size(); i++){
-            mp[arr[i]] = i+1;
+            mp[arr[i]] = i;  
+               // 0 based indexing in .find function since it does key based checking hence ignore value 0 (map does not recognise a key with value 0 so we used .find function)
         }
         for(int j=0; j<arr.size(); j++){
-            if(mp[arr[j]*2] && mp[arr[j] * 2] != j+1) return true;
+            if(mp.find(arr[j]*2) != mp.end() && mp[arr[j] * 2] != j) return true;
         }
         return false;
     }
