48fe9a2e82beee0a80df5bbfa8192ae52de0e63d
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-12-08 16:11:12 +0530

    Time: 3 ms (88.89%), Space: 25.7 MB (11.11%) - LeetHub

---
+++ 3651-transformed-array/3651-transformed-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == 0) result[i] = nums[i];
+
+            else {
+                int steps = nums[i];
+                int targetIndex = ( i + steps % n + n) % n;
+                result[i] = nums[targetIndex];
+            }
+        }
+        return result;
+    }
+};
