a11e0c040d4a47aa10c0b0d2b6ce6437a53f2fc0
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-12-13 22:38:46 +0530

    Time: 0 ms (100%), Space: 22.6 MB (22.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
       // in-place two-pointer technique
       int insert_position = 1; 
       for(int i=1; i<nums.size(); i++){
-        if(nums[i-1] != nums[i]){
+        if(nums[i-1] != nums[i]){       // nums[0] != nums[1] means both unique -> aage i sert krdo fir
             nums[insert_position] = nums[i];
             insert_position++;
         }
