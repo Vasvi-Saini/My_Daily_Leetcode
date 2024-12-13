28e7d9f76c0189146f17205344f1ea70050b12c1
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-12-13 22:44:58 +0530

    Time: 0 ms (100%), Space: 22.7 MB (15.27%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         // set<int> uniques(nums.begin(), nums.end());
-        // can't use since space co plexity is supposed to be O(1)
+        // can't use since space complexity is supposed to be O(1)
       
       // in-place two-pointer technique
       int insert_position = 1; 
       for(int i=1; i<nums.size(); i++){
-        if(nums[i-1] != nums[i]){       // nums[0] != nums[1] means both unique -> aage i sert krdo fir
+        if(nums[i-1] != nums[i]){       // nums[0] != nums[1] means both unique -> aage insert krdo fir
             nums[insert_position] = nums[i];
             insert_position++;
         }
