eecfa6671126f9d30c1d15edb7371b4b39de5ce4
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2024-12-28 23:52:31 +0530

    Time: 0 ms (100%), Space: 31.7 MB (38.52%) - LeetHub

---
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& nums, int k) {
+         reverse(nums.begin() , nums.end());
+
+       for(int i = 0; i<nums.size(); i++){
+         nums[i] += k;
+         k = nums[i]/10;
+         nums[i] %= 10;
+       }
+
+       while(k){
+        nums.push_back(k%10);
+        k/=10;
+       }
+       reverse(nums.begin() , nums.end());
+       return nums;
+    }
+};
