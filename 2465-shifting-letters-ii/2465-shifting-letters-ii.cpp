d49093e57d20ade07fd8ea2ccc26514a99c84545
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-01-05 22:49:22 +0530

    Time: 53 ms (24.17%), Space: 108.7 MB (17.35%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -1,23 +1,23 @@
- class Solution {
+class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         int n = s.size();
-        vector<int> diffArray(n, 0);  
+        vector<int> diffArray(n, 0); 
 
         for (auto shift : shifts) {
-
-            if (shift[2] == 1) {       
-                diffArray[shift[0]]++; 
-                                       
+            if (shift[2] == 1) {      
+                diffArray[shift[0]]++;  
+                                   
                 if (shift[1] + 1 < n) {
-                    diffArray[shift[1] + 1]--;
+                    diffArray[shift[1] + 1]--;  
+                                    
                 }
             } 
-            else {                    // If direction 0
-                diffArray[shift[0]]--;  
+            else {                
+                diffArray[shift[0]]--; 
                                       
                 if (shift[1] + 1 < n) {
-                    diffArray[shift[1] + 1]++;  
+                    diffArray[shift[1] + 1]++; 
                                    
                 }
             }
@@ -27,16 +27,16 @@ public:
         int numberOfShifts = 0;
 
         for (int i = 0; i < s.size(); i++) {
-            numberOfShifts = (numberOfShifts + diffArray[i]) % 26;  
-            
+            numberOfShifts = (numberOfShifts + diffArray[i]) % 26; 
+                                 
             if (numberOfShifts < 0)
-                numberOfShifts +=
-                    26; 
+                numberOfShifts +=  26; 
 
-          
+            
             result[i] = 'a' + (s[i] - 'a' + numberOfShifts) % 26;
         }
 
         return result;
+
     }
 };
