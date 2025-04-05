c4a21e1359c88e7baf72ae6ca0b6fd18800c4597
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-05 22:09:03 +0530

    Time: 0 ms (100%), Space: 65.1 MB (40.98%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -30,15 +30,26 @@ public:
 
 
 // frequency array
-  vector<int> arr(nums.size(), 0);
-  for(int i=0; i<nums.size(); i++){
-    arr[nums[i]]++;
-  }
-  for(int i=0; i<nums.size(); i++){
-    if(arr[i] > 1) return i;
-  }
+//   vector<int> arr(nums.size(), 0);
+//   for(int i=0; i<nums.size(); i++){
+//     arr[nums[i]]++;
+//   }
+//   for(int i=0; i<nums.size(); i++){
+//     if(arr[i] > 1) return i;
+//   }
 
+//   return 0;
+
+int a = 0;
+
+ for(int i=0; i<nums.size(); i++){
+    if(nums[abs(nums[i])] < 0) return abs(nums[i]);
+    nums[abs(nums[i])] = -nums[abs(nums[i])]; 
+  }
+  for(int i=0; i<nums.size(); i++){
+    if(nums[i] < 0) 
+    nums[i] = abs(nums[i]);
+  }
   return 0;
-
     }
 };
