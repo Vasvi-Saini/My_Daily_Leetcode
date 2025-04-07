3811bbf5a0e6628e7c378fe78c85f67caa938f49
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-07 21:43:10 +0530

    Time: 1 ms (81.95%), Space: 14.9 MB (36.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        for(int i=0; i<nums.size()-1; i++){
-            for(int j = i+1; j<nums.size(); j++){
-            if(nums[i] + nums[j] == target) {
-                res.push_back(i);
-                res.push_back(j);
+        // vector<int> res;
+        // for(int i=0; i<nums.size()-1; i++){
+        //     for(int j = i+1; j<nums.size(); j++){
+        //     if(nums[i] + nums[j] == target) {
+        //         res.push_back(i);
+        //         res.push_back(j);
+        //     }
+        // }
+        // }
+        // return res;    O(n^2)
+
+
+
+        //sorting -> O(nlogn) usse index change hojaenge or index hi return krna tha
+
+
+        vector<pair<int,int>> memo;   
+         // hr dbba ek pair h no. or uska idx store hoga poorane wale arr ka  1,1 
+         // vector pair wale element ko sort krne p first element k basis p sort hota h
+
+         vector<int> res;
+
+         for(int i=0; i<nums.size(); i++){
+            memo.push_back({nums[i],i});
+         }
+         sort(memo.begin(), memo.end());
+
+         int left = 0, right = nums.size()-1;
+
+         while(left < right){
+            if(memo[left].first + memo[right].first == target){
+                res.push_back(memo[left].second);
+                res.push_back(memo[right].second);
+                return res;
             }
-        }
-        }
-        return res;
+            else if(memo[left].first + memo[right].first < target) left++;
+            else right--;
+         }
+         return res;
+
     }
 };
