8c51d8bc875672434079801b10a44ec3f1bcc53d
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-08 10:01:06 +0530

    Time: 127 ms (6.94%), Space: 14.1 MB (77.58%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,69 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // vector<int> res;
-        // for(int i=0; i<nums.size()-1; i++){
-        //     for(int j = i+1; j<nums.size(); j++){
-        //     if(nums[i] + nums[j] == target) {
-        //         res.push_back(i);
-        //         res.push_back(j);
-        //     }
-        // }
-        // }
-        // return res;    O(n^2)
-
-
-
-        //sorting -> O(nlogn) usse index change hojaenge or index hi return krna tha
-
-
-        // vector<pair<int,int>> memo;   
-         // hr dbba ek pair h no. or uska idx store hoga poorane wale arr ka  1,1 
-         // vector pair wale element ko sort krne p first element k basis p sort hota h
-
-        //  vector<int> res;
-
-        //  for(int i=0; i<nums.size(); i++){
-        //     memo.push_back({nums[i],i});
-        //  }
-
-        //  sort(memo.begin(), memo.end());
-
-        //  int left = 0, right = nums.size()-1;
-
-        //  while(left < right){
-        //     if(memo[left].first + memo[right].first == target){
-        //         res.push_back(memo[left].second);
-        //         res.push_back(memo[right].second);
-        //         return res;
-        //     }
-        //     else if(memo[left].first + memo[right].first < target) left++;
-        //     else right--;
-        //  }
-
-        //  return res;
-
-
-
-        //using hashmap
-
-        unordered_map<int, int> map;
+        // 1st approach tc n^2
+        int n = nums.size();
         vector<int> res;
-
-        for(int i=0; i<nums.size(); i++){
-            int a = nums[i];
-            int b = target - a;
-            if(map.find(b) != map.end()){
-                res.push_back(i);
-                res.push_back(map[b]);
-                return res;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i] + nums[j] == target){
+                     res.push_back(i);
+                     res.push_back(j);
+                }
             }
-            map[a] = i;
         }
-
-
-      return res;
-
-
+        return res;
     }
 };
