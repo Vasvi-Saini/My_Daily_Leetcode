547f490d7091b5bf8ecf1c996c4bf423e402a80a
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-10 12:57:03 +0530

    Time: 11 ms (56.55%), Space: 21.1 MB (33.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
 
-    int countPalindromes(string s , int i , int j){
-        int count = 0;
+    int palindromes(string s, int left, int right){
+         int count = 0;
 
-        while(i >= 0 && j < s.size()){
-            if(s[i--] == s[j++]) count++;
+        while(left>=0 && right<=s.size()-1){
+          
+            if(s[left] == s[right]){
+                count++;
+            }
             else break;
+            left--;
+            right++;
         }
+        
         return count;
+     
     }
-    
+
     int countSubstrings(string s) {
         int count = 0;
-        for(int i = 0; i<s.size(); i++){
-                count += countPalindromes(s , i , i);
-                count += countPalindromes(s, i, i+1);
+        for(int i=0; i<s.size(); i++){
+            count += palindromes(s, i, i);
+            count += palindromes(s, i, i+1);
         }
         return count;
     }
