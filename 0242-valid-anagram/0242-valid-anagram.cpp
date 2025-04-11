ed88a506ef504cee307e88284e9fe8a8b12a6118
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-11 21:21:17 +0530

    Time: 7 ms (22.05%), Space: 9.5 MB (98.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,37 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // int v1 = 0;
-        // int v2 = 0;
-        // if (s.length() != t.length()) return false;
-        // for (auto c : s) {
-        //     v1 ^= 1 << ('z' - c);
-        // }
-        // for (auto c : t) {
-        //     v2 ^= 1 << ('z' - c);
-        // }
-        // return v1 == v2;
-
-
-        map<char, int> memo;
-        for (auto c : s) {
-            if (memo[c]) {
-                memo[c]++;
-            } else {
-                memo[c] = 1;
-            }
-        }
-
-        for (auto c : t) {
-            if (memo[c]) {
-                memo[c]--;
-            } else {
-                memo[c] = -1;
-            }
-        }
-      for(auto &c : memo){
-        if(c.second != 0)   return false;
-   }
-   return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t) return true;
+        return false;
     }
 };
