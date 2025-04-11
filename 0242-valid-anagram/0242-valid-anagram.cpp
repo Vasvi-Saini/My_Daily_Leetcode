de6fba08e43826f42813c2f1f1a7923afe8053e5
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-04-11 21:35:07 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t) return true;
-        return false;
+        // nlogn
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if(s == t) return true;
+        // return false;
+
+        // freq arr tc: O(n)
+    vector<int> arr(256, 0);
+
+    for(int i=0; i<s.size(); i++){
+        arr[s[i]]++;
+    }
+    for(int i=0; i<t.size(); i++){
+        arr[t[i]]--;
+    }
+    for(int i=0; i<256; i++){
+    if(arr[i] != 0) return false;
+    }
+    return true;
     }
 };
