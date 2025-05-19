0c8a90251137656cab81deac053bba64f14c0426
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-05-19 14:28:58 +0530

    Time: 0 ms (100%), Space: 22.7 MB (88.05%) - LeetHub

--- 3321-type-of-triangle/3321-type-of-triangle.cpp
+++ 3321-type-of-triangle/3321-type-of-triangle.cpp
@@ -9,14 +9,15 @@ public:
         return 0;
     }
     bool isscalene(vector<int>& nums) {
-        if(nums[0] != nums[1] != nums[2]) return 1;
+        if(nums[0] != nums[1] && nums[1] != nums[2]) return 1;
         return 0;
     }
     string triangleType(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         if(nums[0] +nums[1] <= nums[2]) return "none";
         if(isEquilateral(nums)) return "equilateral";
-        if(isisosceles(nums)) return "isosceles";
-        return "scalene";
+         if(isscalene(nums)) return "scalene";
+         return "isosceles";
+       
     }
 };
