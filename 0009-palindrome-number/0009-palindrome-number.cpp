88e0608af26b41da016bef6dfee706f331941cf8
Vasvi Saini <156455045+Vasvi-Saini@users.noreply.github.com>
2025-05-24 18:15:45 +0530

    Time: 1 ms (60.17%), Space: 8.4 MB (92.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,14 +4,15 @@ public:
         if(x<0) return 0;
         int rem = 0;
         int sum = 0;
-        int temp = x;
-        while(temp!=0){
-           rem = temp%10;
-            temp/=10;
+        int n = x;
+        while(n){
+            rem = n%10;
+            n /= 10;
             if(sum > INT_MAX/10) return 0;
             sum = sum*10 + rem;
         }
         if(sum == x) return 1;
-        else return 0;
+        return 0;
+
     }
 };
