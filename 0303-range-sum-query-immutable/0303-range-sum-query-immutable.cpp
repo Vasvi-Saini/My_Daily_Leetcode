757ab08493b7f7bb22784cd657e07c200dced6f3
Vasvi Saini <vasvisaini60@gmail.com>
2025-06-06 11:42:41 +0530

    Time: 0 ms (100%), Space: 24 MB (24.07%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -3,11 +3,12 @@ public:
     vector<int> prefix;
 
     NumArray(vector<int>& nums) {
-        prefix.resize(nums.size());
-        prefix[0] = nums[0];
-        for(int i=1; i<nums.size(); i++){
-            prefix[i] = prefix[i-1] + nums[i];
+        int sum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            sum += nums[i];
+            prefix.push_back(sum);
         }
+
     }
     
     int sumRange(int left, int right) {
