673b85bd6c7ca067f150f0ce52ee734b5128480d
Vasvi Saini <vasvisaini60@gmail.com>
2025-06-09 17:47:56 +0530

    Time: 3 ms (48.79%), Space: 24 MB (24.16%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,24 +1,27 @@
 class NumArray {
 public:
+
     vector<int> prefix;
 
     NumArray(vector<int>& nums) {
-        int sum = 0;
-        for(int i = 0; i<nums.size(); i++){
-            sum += nums[i];
-            prefix.push_back(sum);
+        prefix.resize(nums.size());
+        prefix[0] = nums[0];
+
+        for(int i = 1; i<nums.size(); i++){
+          prefix[i] = prefix[i-1] + nums[i];
         }
 
     }
     
+    
+    
     int sumRange(int left, int right) {
         // Prefix_Sum arr m hmare pass 0-1 0-2 0-3 0-4 tk kam sum h pr agr left 0 ho hi na toh
         if(left > 0 ){
-
             return prefix[right] - prefix[left-1]; // left wala minus krdo 
-
         }
-        return prefix[right];    // exact 0 to si=omething h toh return krdo
+
+        return prefix[right];    // exact 0 to something h toh return krdo
     }
 };
 
