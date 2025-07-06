643022f2ae27e38aa9cea4c846523aa40207c4cc
Vasvi Saini <vasvisaini60@gmail.com>
2025-07-06 23:26:56 +0530

    Time: 2 ms (50.44%), Space: 8.8 MB (69.34%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+      string s = "a";
+        while(s.size() < k){
+            int n = s.size();
+            int i = 0;
+            while(i<n && s.size() < k){   
+                s += s[i]+1;
+                i++;
+            }
+        }
+        return s[s.size()-1];
+        
+
+    }
+};
