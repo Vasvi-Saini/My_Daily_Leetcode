d62e5cc6d418e9e50414f35b7a4d6d8ec3889f34
Vasvi Saini <vasvisaini60@gmail.com>
2025-07-25 00:13:40 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.39%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -16,7 +16,7 @@ public:
         if (p == NULL || q == NULL) {
             return false;
         }
-        if(p->val == q->val) return isSameTree(p->left , q->left) && isSameTree(p->right ,    q->right);
+        if(p->val == q->val) return isSameTree(p->left , q->left) && isSameTree(p->right , q->right);
         return 0;
     }
 };
