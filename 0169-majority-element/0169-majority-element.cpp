c1721d96be91aad39e1d1faeac39552f96501452
Vasvi Saini <vasvisaini60@gmail.com>
2025-08-28 23:59:01 +0530

    Time: 4 ms (20.1%), Space: 28.4 MB (7.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,14 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
         unordered_map<int, int> mp;
 
-        int res = 0; // how many times the number has appeared
-        int majority_num = 0; // to store number that appears majority number of times
+        int res = 0; // to store number that appears majority number of times
+        int majority_count = 0; // how many times the number has appeared
+        
 
         for (int i : nums) {
             mp[i]++;
-            if (mp[i] > majority_num) {
+            if (mp[i] > majority_count) {
                 res = i;
-                majority_num = mp[i];
+                majority_count = mp[i];
             }
         }
         return res;
