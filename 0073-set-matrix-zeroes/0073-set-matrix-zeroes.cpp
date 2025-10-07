24c2a2f10d40d7b0ab3013eaa598e233b1edf21f
Vasvi Saini <vasvisaini60@gmail.com>
2025-10-07 13:33:06 +0530

    Time: 0 ms (100%), Space: 19 MB (5.63%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,56 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool rowHasZero = false;
-        bool colHasZero = false;
+       int r = matrix.size();
+       int c = matrix[0].size();
+       vector<vector<int>> mat1(r, vector<int>(c));
+       for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+          mat1[i][j] = matrix[i][j];
+        }
+       }
 
-        // checking if first row has 0
-        for(int i=0; i<matrix[0].size(); i++){    // matrix[0].size() gives no. of columns in first row of matrix.
-            if(matrix[0][i] == 0){
-                rowHasZero = true;
-                break;
+       for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            if(mat1[i][j] == 0){
+                for(int a = 0; a<r; a++) matrix[a][j] = 0;
+                for(int b=0; b<c; b++) matrix[i][b] = 0;
             }
         }
+       }
 
-        // checking if first column has 0
-        for(int i=0; i<matrix.size(); i++){
-            if(matrix[i][0] == 0){
-                colHasZero = true;
-                break;
-            }
-        }
-    
-        // checking 0 in rest of array
-        for(int i=1; i < matrix.size(); i++){
-            for(int j=1; j < matrix[i].size(); j++){
-                if(matrix[i][j] == 0){
-                matrix[0][j] = 0;
-                matrix[i][0] = 0;
-            }
-        }
-    }
 
-        for(int i=1; i < matrix.size(); i++){             // 3 3 matrix
-            for(int j=1; j < matrix[i].size(); j++){   //  matrix[i].size() no columns in i-th row of 2D Arr matrix
-                if(matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-      // set all the elements in row to 0
-      if(rowHasZero){
-        for(int i=0; i<matrix[0].size(); i++){
-            matrix[0][i] = 0;
-        }
-      }
-
-       // set all the elements in column to 0
-      if(colHasZero){
-        for(int i=0; i<matrix.size(); i++){
-            matrix[i][0] = 0;
-        }
-      }
 
     }
 };
