d0b65a45c7c54c6378191ec4041b95ee9354c119
Vasvi Saini <vasvisaini60@gmail.com>
2025-10-16 09:53:01 +0530

    Time: 0 ms (100%), Space: 19.1 MB (5.65%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,31 +3,27 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-        bool rowZero = 0, colZero = 0;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (!matrix[i][j]) {
-                      if (i == 0)
-                        rowZero = 1;
-                    if (j == 0)
-                        colZero = 1;
-                    matrix[0][j] = 0;
-                    matrix[i][0] = 0;
-                  
-                }
+        vector<vector<int>> copy(r, vector<int>(c));
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                copy[i][j] = matrix[i][j];
             }
         }
 
-        for(int i = 1; i<r; i++){
-            for(int j=1; j<c; j++){
-                if(!matrix[0][j] || !matrix[i][0]){
-                    matrix[i][j] = 0;
+        for(int i = 0; i<r; i++){
+            for(int j=0; j<c; j++){
+            if(copy[i][j] == 0){
+                for(int a = 0; a <r; a++){
+                    matrix[a][j] = 0;
                 }
+                for(int b = 0; b<c; b++){
+                    matrix[i][b] = 0;
+                }
+            }
             }
         }
 
-        if(rowZero) for(int j = 0; j<c; j++) matrix[0][j] = 0;
-        if(colZero) for(int i = 0; i<r; i++) matrix[i][0] = 0;
     }
 };
