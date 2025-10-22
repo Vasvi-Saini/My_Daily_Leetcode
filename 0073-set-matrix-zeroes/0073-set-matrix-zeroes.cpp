8ae7c2ed4d09ee3dd06b4c7ccf8d7d3d0fa0b86e
Vasvi Saini <vasvisaini60@gmail.com>
2025-10-22 23:46:34 +0530

    Time: 0 ms (100%), Space: 18.5 MB (78.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,31 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int r = matrix.size();
         int c = matrix[0].size();
-        bool rowZero = 0, colZero = 0;
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (!matrix[i][j]) {
-                      if (i == 0)
-                        rowZero = 1;
-                    if (j == 0)
-                        colZero = 1;
-                    matrix[0][j] = 0;
+        bool row = 0, col = 0;
+
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(!matrix[i][j]){
+                    if(i==0) row = 1;
+                    if(j==0) col = 1;
                     matrix[i][0] = 0;
-                  
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i = 1; i<r; i++){
+        for(int i=1; i<r; i++){  // baaki ki matrix ko 0 krdo 1 idx k baad wali jo 
             for(int j=1; j<c; j++){
-                if(!matrix[0][j] || !matrix[i][0]){
-                    matrix[i][j] = 0;
-                }
+                if(matrix[0][j]==0 || matrix[i][0] == 0) matrix[i][j] = 0;
             }
         }
 
-        if(rowZero) for(int j = 0; j<c; j++) matrix[0][j] = 0;
-        if(colZero) for(int i = 0; i<r; i++) matrix[i][0] = 0;
+       if(row){
+        for(int j=0; j<c; j++) matrix[0][j] = 0;
+       }
+       if(col){
+        for(int i=0; i<r; i++) matrix[i][0] = 0;
+       }
+
     }
 };
