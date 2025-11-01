5687c45afd647bec3dea2e56d5b22386d078bf81
Vasvi Saini <vasvisaini60@gmail.com>
2025-11-01 16:26:45 +0530

    Time: 0 ms (100%), Space: 14.8 MB (51.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // 3rd approach hashmap tc O(n)
-        unordered_map<int, int> map;
-        int n = nums.size();
+        // 2nd approach tc O(nlog n) sort
+        vector<pair<int, int>> memo;
+
         vector<int> res;
 
+        int n = nums.size();
+
         for(int i=0; i<n; i++){
-            int a = nums[i];
-            int b = target - a;
-            if(map.find(b) != map.end()){   // vo chiz mil gyi h
-                res.push_back(i);
-                res.push_back(map[b]); // b ka index 
-                return res;
-            }
-            map[a] = i; // agr nhi milta toh
+            memo.push_back({nums[i], i});
         }
 
-       return res;
+        sort(memo.begin(), memo.end());
 
+        int left = 0, right = nums.size()-1;
+
+        while(left < right){
+            if(memo[left].first + memo[right].first == target){
+                res.push_back(memo[left].second);
+                res.push_back(memo[right].second);
+                return res;
+            } 
+            else if(memo[left].first + memo[right].first < target) left++;
+            else right--;
+        }
+    return res;
     }
 };
