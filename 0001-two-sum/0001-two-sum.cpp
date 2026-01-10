4d383f6a3bb5259832d044c4e1761ab7f57af9b4
Vasvi Saini <vasvisaini60@gmail.com>
2026-01-10 19:03:15 +0530

    Time: 37 ms (29.87%), Space: 14 MB (80.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,15 @@
 class Solution {
 public:
+// o(n2)
     vector<int> twoSum(vector<int>& nums, int target) {
-        // 2nd approach tc O(nlog n) sort
-        vector<pair<int, int>> memo;
-
-        vector<int> res;
-
         int n = nums.size();
-
         for(int i=0; i<n; i++){
-            memo.push_back({nums[i], i});
+            for(int j=i+1; j<n; j++){
+                if(nums[i]+ nums[j] ==  target){
+                    return {i,j};
+                }
+            }
         }
-
-        sort(memo.begin(), memo.end());
-
-        int left = 0, right = nums.size()-1;
-
-        while(left < right){
-            if(memo[left].first + memo[right].first == target){
-                res.push_back(memo[left].second);
-                res.push_back(memo[right].second);
-                return res;
-            } 
-            else if(memo[left].first + memo[right].first < target) left++;
-            else right--;
-        }
-    return res;
+        return {};
     }
 };
