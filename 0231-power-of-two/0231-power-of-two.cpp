b345c17b6e92759e25060140fc68fa301e765bc0
Vasvi Saini <vasvisaini60@gmail.com>
2026-01-15 18:48:22 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.06%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n<=0) return 0;
-        
-        return __builtin_popcount(n) == 1;
+       if(n<=0) return 0;
+       while(n%2==0){
+        n/=2;
+       } 
+       return n==1;
     }
-};  
+};
