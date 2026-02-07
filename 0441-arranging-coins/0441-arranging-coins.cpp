3e5de90135ff1d91e31346736bb94e4f5a1f1312
Vasvi Saini <vasvisaini60@gmail.com>
2026-02-08 00:10:34 +0530

    Time: 2 ms (36.57%), Space: 8.8 MB (68.08%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int arrangeCoins(int n) {
         int row = 1;
-        while(n >=  row){
-            n -= row;
-            row++;
+        while(n >=  row){    // if i have coin equals curr row
+            n -= row;       // curr row m coins filled
+            row++;          // row updated to fill next row equals coins
         }
-        return row - 1;
+        return row - 1;     // row must have moved to one step ahead where the conditipn has terminated
     }
 };
