55c142cf3776ab6c939e7eb16f5d905176ecf918
Vasvi Saini <vasvisaini60@gmail.com>
2026-02-08 18:55:37 +0530

    Time: 0 ms (100%), Space: 9.8 MB (32.21%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+      if(str1 + str2 != str2 + str1) return "";
+      int str_len = gcd(str1.length(), str2.length());
+        string ans = str1.substr(0,str_len);
+       return ans;
+    }
+};
