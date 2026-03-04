cd89927c892e551c2f491ecab63119ad176dabe5
Vasvi Saini <vasvisaini60@gmail.com>
2026-03-04 23:11:23 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.23%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count=0;
-        for(int i = s.length()-1; i>=0; i--){
-            if(s[i]!=' ') count++;
-            else if(count>0) return count;
+        int count = 0;
+        for(int i=s.size()-1; i>=0; i--){
+            if(s[i] != ' ') count++;
+            else if(count > 0) return count; // it means i encounter a space after the count is not zero thaat ultimately means the last word has ended
         }
         return count;
     }
