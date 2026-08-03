0050f916e97a0d3efb2c7bfe9943b3dc5b6cb77f
Vasvi Saini <vasvisaini60@gmail.com>
2026-08-04 01:10:24 +0530

    Time: 0 ms (100%), Space: 65.1 MB (42.15%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,55 +1,12 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // for(int i=0; i<nums.size(); i++){
-        //     if(nums[abs(nums[i])] < 0) return abs(nums[i]);
-        //     nums[abs(nums[i])] = -nums[abs(nums[i])];
-        // }
-        // return -1;
-
-
-
-        // for(int i=0; i<nums.size()-1; i++){
-        //     for(int j=i+1; j<nums.size(); j++){
-        //         if(nums[i] == nums[j]) return nums[i];
-        //     }
-        // }  // y tle degaS
-        // return 0;
-
-
-// sort krke  O(nlogn )
-
-// sort(nums.begin(), nums.end());
-
-//     for(int i=0; i<nums.size()-1; i++){ // second last element tk chlega
-//    if(nums[i] == nums[i+1]) return  nums[i];
-  
-//   }
-// return 0;
-
-
-
-// frequency array
-//   vector<int> arr(nums.size(), 0);
-//   for(int i=0; i<nums.size(); i++){
-//     arr[nums[i]]++;
-//   }
-//   for(int i=0; i<nums.size(); i++){
-//     if(arr[i] > 1) return i;
-//   }
-
-//   return 0;
-
-int a = 0;
-
- for(int i=0; i<nums.size(); i++){
-    if(nums[abs(nums[i])] < 0) return abs(nums[i]);
-    nums[abs(nums[i])] = -nums[abs(nums[i])]; 
-  }
-  for(int i=0; i<nums.size(); i++){
-    if(nums[i] < 0) 
-    nums[i] = abs(nums[i]);
-  }
-  return 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[abs(nums[i])] < 0) {  // if n0. is -ve  means already visited and now visiting seond tym 
+                return abs(nums[i]);
+            }
+           nums[abs(nums[i])]  = - nums[abs(nums[i])];   // first tym visited then make it -ve
+        }
+        return 0;
     }
 };
