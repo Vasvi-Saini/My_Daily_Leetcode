528778ffaf97f35f91fef5d79a7487a160fb67c2
Vasvi Saini <vasvisaini60@gmail.com>
2026-08-04 23:42:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0;
         for(int i=s.size()-1; i>=0; i--){
             if(s[i] != ' ') count++;
-            else if(count > 0) return count; // it means i encounter a space after the count is not zero thaat ultimately means the last word has ended
+             else if(count > 0) return count;
         }
-        return count;
+        return 0;
     }
 };
