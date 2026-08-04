9cd834757882d6ec33a0bf00bc59d5471c9bbda6
Vasvi Saini <vasvisaini60@gmail.com>
2026-08-04 21:46:39 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.51%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-       int ans = 0;
-       for(int i=5; i<=n; i=i*5){
-        ans += n/i;
-       } 
-       return ans;
+        int count = 0;
+        while(n > 0){
+            n /= 5;
+            count += n;
+        }
+        return count;
     }
 };
